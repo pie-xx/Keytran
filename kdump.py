2a7bebd7e74f877f384d5a3da672f25d510c0dff
Kajikawa, Takafumi <piecrosoft@gmail.com>
2021-02-02 22:23:19 +0900

    HIDシーケンスをダンプするようにした

--- kdump.py
+++ kdump.py
@@ -3,6 +3,25 @@ from evdev import *
 
 class Keyboard():
 	def __init__(self):
+		self.state=[
+			0xA1, #this is an input report
+			0x01, #Usage report = Keyboard
+			#Bit array for Modifier keys
+			[0,	#Right GUI - Windows Key
+			 0,	#Right ALT
+			 0, 	#Right Shift
+			 0, 	#Right Control
+			 0,	#Left GUI
+			 0, 	#Left ALT
+			 0,	#Left Shift
+			 0],	#Left Control
+			0x00,	#Vendor reserved
+			0x00,	#rest is space for 6 keys
+			0x00,
+			0x00,
+			0x00,
+			0x00,
+			0x00]
 
 		print ("waiting for keyboard")
 
@@ -22,8 +41,30 @@ class Keyboard():
 		
 	def event_loop(self):
 		for event in self.dev.read_loop():
-			print(event)
+			self.change_state(event)
+			print(self.state)
 
+	def change_state(self,event):
+		evdev_code=ecodes.KEY[event.code]
+		modkey_element = keymap.modkey(evdev_code)
+		
+		if modkey_element > 0:
+			if self.state[2][modkey_element] ==0:
+				self.state[2][modkey_element]=1
+			else:
+				self.state[2][modkey_element]=0
+		else:
+			#Get the keycode of the key
+			hex_key = keymap.convert(ecodes.KEY[event.code])
+			#Loop through elements 4 to 9 of the inport report structure
+			for i in range(4,10):
+				if self.state[i]== hex_key and event.value ==0:
+					#Code 0 so we need to depress it
+					self.state[i] = 0x00
+				elif self.state[i] == 0x00 and event.value==1:
+					#if the current space if empty and the key is being pressed
+					self.state[i]=hex_key
+					break;
 
 if __name__ == "__main__":
 	print ("Setting up keyboard")
