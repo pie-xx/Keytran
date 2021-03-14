950d313d7f131179aa9f5f68ed837251fb0e5ebe
Kajikawa, Takafumi <piecrosoft@gmail.com>
2021-03-15 02:07:26 +0900

    修飾キーは先に押しておくようにした

--- HIDkey.py
+++ HIDkey.py
@@ -72,8 +72,11 @@ class USBHIDkey():
 
     def putKey(self, mod, code):
         kbuff = bytes([mod, 0, code, 0,0,0,0,0])
+        mbuff = bytes([mod, 0, 0, 0,0,0,0,0])
         wait=0.02
         with open('/dev/hidg1', 'wb') as f:
+            f.write( mbuff )
+            time.sleep(wait)
             f.write( kbuff )
             time.sleep(wait)
             f.write( self.offbuff )
